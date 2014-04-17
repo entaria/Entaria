c4bd607300403dfc9cb53655f2aa828efbbd23d2
jhugh <jhugh@github.com>
2014-04-17 18:07:07 +0100

    unignoring files

---
+++ Entaria/Abstract/IAdminRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entaria.Models;
+
+namespace Entaria.Abstract
+{
+    interface IAdminRepository
+    {
+        IQueryable<Admin> Admins { get; }
+    }
+}
