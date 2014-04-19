e22bb0134242ab3c0765d5c0dcd53461280d351c
adrianmann <adrianmatthiasmann@hotmail.com>
2014-04-19 10:28:13 +0100

    Adrian's 19/04/14

--- Entaria/Controllers/LCHRegistrationController.cs
+++ Entaria/Controllers/LCHRegistrationController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Entaria.Models;
 
 namespace Entaria.Controllers
 {
